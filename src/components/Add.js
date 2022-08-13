a1b25945add0ba9679361694ac77f114c65ee7c2
TAATA EARL <fredvuni809@gmail.com>
2022-08-13 17:15:32 +0300

    Added cors mode to the api endpoint

--- src/components/Add.js
+++ src/components/Add.js
@@ -11,7 +11,7 @@ const Add = () => {
         e.preventDefault()
         setQuery(e.target.value)
         
-        fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&query=${e.target.value}&page=1&include_adult=false`)
+        fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&query=${e.target.value}&page=1&include_adult=false`,{mode:'cors'})
         .then((res)=> res.json())
         .then((data)=>{
             if(!data.errors){
